6cd00ef981a469e52a91db9a1ce3fd29af3b3316
lengmingxuan <983835745@qq.com>
2019-10-21 22:52:18 +0800

    The first commit

---
+++ Class management system/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Class management system
+//
+//  Created by Mingxuan Leng on 2019/10/21.
+//  Copyright © 2019 Leng Mingxuan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
